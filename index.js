8d2d32a42f71f67859742ff9c5dd1c4b6dbe0ab5
vijetha-kini-20 <141136769+vijetha-kini-20@users.noreply.github.com>
2026-01-12 17:40:33 +0530

    Add files via upload
    hh

---
+++ index.js
@@ -0,0 +1,20 @@
+//CALCULATOR PROGRAM
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(input){
+    display.value = "";
+}
+
+function calculate(input){
+    try{
+        display.value = eval(display.value);
+    }
+   catch(error){
+    display.value = "error"
+   }
+}
